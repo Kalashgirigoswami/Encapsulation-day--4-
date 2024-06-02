ef99269933304246d38ff3facadd04a86f9683a3
Kalashgirigoswami <168511033+Kalashgirigoswami@users.noreply.github.com>
2024-06-02 11:01:47 +0530

    Add files via upload

---
+++ Cout.java
@@ -0,0 +1,20 @@
+ class Cout{
+    private String name;
+    private String fathername;
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public void setFathername(String fathername) {
+         this.fathername = fathername;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public String getFathername() {
+         return fathername;
+     }
+ }
---
+++ Kk.java
@@ -0,0 +1,10 @@
+class Kk{
+    public static void main(String args[])
+    {
+        Cout c1=new Cout();
+        c1.setName("mukesh");
+        c1.setFathername("ajay");
+        System.out.println("my name:"+c1.getName());
+        System.out.println("my father name:"+c1.getFathername());
+    }
+}
